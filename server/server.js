e297939679e8d42a6fc75b2b554e129dfb173946
Kenneth <gitfupanda@outlook.com>
2016-11-20 21:40:53 +1300

    Added getting a single todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +29,27 @@ app.get('/todos', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error);
     });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;    
+    
+    
+    if (!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    else {
+        Todo.findById(id).then((todo) => {
+            if (!todo){
+                return res.status(404).send();
+            }
+            res.status(200).send({todo});
+        }).catch((error) => {
+            res.status(400).send();
+        });
+    }
+     
 });
 
 
