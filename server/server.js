48349f9468f4c51bcf36da377e682fb6e23a5ea6
Kenneth <gitfupanda@outlook.com>
2016-11-21 18:23:23 +1300

    Prep for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
@@ -53,8 +55,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Express started on port 3000');
+app.listen(port, () => {
+    console.log(`Express started on port ${port}`);
 });
 
 module.exports = {app};
