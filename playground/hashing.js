2df56b5ad75144362348306c5ec7a3c473019302
Kenneth <gitfupanda@outlook.com>
2016-11-23 09:49:09 +1300

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 4
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
+
+var hashedPassword = '$2a$10$34DZeNtjKziCtTElAgYYDuSq9ZYd5DgwHBowXsUHUpVtxVszYKSIm';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+})
+
+// var data = {
+//     id: 4
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var message = 'I am user 5';
 // var hash = SHA256(message).toString();
