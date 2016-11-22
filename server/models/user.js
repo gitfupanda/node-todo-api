9a7dbaa9bc834de476e32e60ea1493d60e51b791
Kenneth <gitfupanda@outlook.com>
2016-11-22 21:50:14 +1300

    Added Auth Middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,6 +45,24 @@ UserSchema.methods.generateAuthToken = function (){
     })
 };
 
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    }
+    catch (ex){
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
 UserSchema.methods.toJSON = function (){
     var user = this;
     var userObject = user.toObject();
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 var port = process.env.PORT;
@@ -119,6 +120,11 @@ app.post('/users', (req, res) => {
     })
 });
 
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port, () => {
     console.log(`Express started on port ${port}`);
