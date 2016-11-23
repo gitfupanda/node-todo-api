0871bcb8a0ffec65d9f20bc1b69fc04d7e1ea977
Kenneth <gitfupanda@outlook.com>
2016-11-23 21:12:57 +1300

    Added DELETE token endpoint

--- server/server.js
+++ server/server.js
@@ -132,7 +132,15 @@ app.post('/users/login', (req, res) => {
         res.status(400).send();
     })
 
-})
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }).catch((ex) => {
+        res.status(400).send();
+    });
+});
 
 
 
